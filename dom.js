d7ab62a92f67425a0716bd471bb10d754a6291e0
Kavi011 <88363985+Kavi011@users.noreply.github.com>
2021-08-05 13:58:46 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,11 @@
+
+function typing(){
+
+    let name=document.querySelector("#inp").value;
+    let city=document.querySelector("#inp1").value;
+    
+    console.log(document.querySelector("#sec").innerText =name, city);
+    // document.querySelector("#sec").innerText = city;
+    
+}
+
